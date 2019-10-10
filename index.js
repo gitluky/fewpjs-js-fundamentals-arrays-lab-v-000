6160fd6e9e5f960ed4e028463f22bca2d5cec5b0
Jonathan Luk <jluk87@gmail.com>
2019-10-10 17:02:20 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Write your solution here!
+
+const append = function() {
+  const pets = ["Milo", "Otis", "Garfield"];
+  return pets.push("Odie");
+}
+
+const prepend = function() {
+  const pets = ["Milo", "Otis", "Garfield"];
+  return pets.unshift("Odie");
+}
+
+const removeLast = function() {
+  const pets = ["Milo", "Otis", "Garfield"];
+  return pets.pop();
+}
+
+const removeFirst() {
+  const pets = ["Milo", "Otis", "Garfield"];
+  return pets.shift()
+}
